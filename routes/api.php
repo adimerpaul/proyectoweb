5b2e5b579b349e1f90a20ed3eaa20f071cc0a28d
adimerpaul <adimer101@email.com>
2021-10-01 19:52:44 -0400

    proyecto terminado

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login',[\App\Http\Controllers\UserController::class,'login']);
 
 Route::group(['middleware'=>'auth:sanctum'],function (){
+    Route::apiResource('/teacher',\App\Http\Controllers\TeacherController::class);
     Route::post('/logout',[\App\Http\Controllers\UserController::class,'logout']);
     Route::post('/me',[\App\Http\Controllers\UserController::class,'me']);
     Route::get('/student',[\App\Http\Controllers\StudentController::class,'index']);
@@ -31,6 +32,6 @@ Route::group(['middleware'=>'auth:sanctum'],function (){
     Route::put('/student/{student}',[\App\Http\Controllers\StudentController::class,'update']);
     Route::post('/student',[\App\Http\Controllers\StudentController::class,'store']);
     Route::delete('/student/{student}',[\App\Http\Controllers\StudentController::class,'destroy']);
-    Route::apiResource('/teacher',\App\Http\Controllers\TeacherController::class);
+
 });
 
